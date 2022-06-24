d4121a3f207485a9c2986b187495568a1e290227
tati oliveira <tat1roliv@gmail.com>
2022-06-24 08:58:12 -0300

    css cards

--- event-platform/src/components/Lesson.tsx
+++ event-platform/src/components/Lesson.tsx
@@ -1,7 +1,21 @@
+import { CheckCircle } from 'phosphor-react'
+
 export function Lesson(){
     return(
     <a href="#">
-        <span>Terça • 22 de junho • 19h00</span>
+        <span className="text-gray-300">
+            Terça • 22 de junho • 19h00
+        </span>
+        <div className="rounded border border-gray-500 p-4 mt-2">
+            <header className="flex items-center justify-between">
+                <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                    <CheckCircle size={20} />
+                    Em breve
+                </span>
+                <span className="text-xs rounded px-2 py-[2px] text-white border border-green-300 font-bold">aula prática</span>
+            </header>
+            <strong className="text-gray-200 mt-5">Aula 02 - Titulo aula ignite labs</strong>
+        </div>
     </a>
     );
 }
