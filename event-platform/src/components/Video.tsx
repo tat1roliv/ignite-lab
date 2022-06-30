0990403bac7aa3cf047a9ea169a606e8cd480594
tati oliveira <tat1roliv@gmail.com>
2022-06-30 08:56:03 -0300

    Update Video.tsx

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,7 +1,71 @@
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+
 export function Video(){
     return(
     <div className="flex-1">
-        video
+
+        <div className="bg-black flex justify-center">
+            <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        </div>
+
+        <div className="p-8 max-w-[1100px] mx-auto">
+            <div className="flex items-start gap-16">
+                <div className="flex-1">
+                    <h1 className="text-2xl font-bold">Aula 1</h1>
+                    <p className="mt-4 text-gray-200 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda illo ea, error impedit maxime voluptate porro recusandae aperiam provident earum facilis consequatur voluptatem qui magni, officiis natus. Accusantium, vel totam.</p>
+                    <div className="flex items-center gap-4 mt-6">
+                        <img 
+                            className="w-16 h-16 rounded-full border-2 border-blue-500"
+                            src="https://tatioliveira.tech/assets/images/1.png" 
+                            alt="image" />
+                        <div className="leading-relaxed">
+                            <strong className="font-bold text-2xl block">Professor</strong>
+                            <span className="text-sm text-gray-200 block">Work at</span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-4">
+                    <a href="" className="p-4 text-sm border bg-green-500 flex items-center rounded font-bold gap-2 uppercase justify-center hover:bg-green-700 transition-colors">
+                        <DiscordLogo size={24}/>
+                        Comunidade do Discord 
+                    </a>
+                    <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold gap-2 uppercase justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                        <Lightning size={24}/>
+                        Acesse o desafio
+                    </a>
+                </div>
+            </div>
+
+            <div className="gap-8 mt-20 grid grid-cols-2">
+                <a href="https://" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <div className="bg-green-700 h-full p-6 flex items-center">
+                        <FileArrowDown size={40}/>
+                    </div>
+                    <div className="py-6 leading-relaxed">
+                        <strong className="text-xl">Material complementar</strong>
+                        <p className="text-sm text-gray-200 mt-2"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                    <div className="h-full p-6 flex items-center">
+                        <CaretRight size={24} />
+                    </div>
+                </a>
+                <a href="https://" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <div className="bg-green-700 h-full p-6 flex items-center">
+                        <FileArrowDown size={40}/>
+                    </div>
+                    <div className="py-6 leading-relaxed">
+                        <strong className="text-xl">Materiais exclusivos</strong>
+                        <p className="text-sm text-gray-200 mt-2"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                    <div className="h-full p-6 flex items-center">
+                        <CaretRight size={24} />
+                    </div>
+                </a>
+            </div>
+        </div>
+
     </div>
     );
 }
