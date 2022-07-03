34e0cbc766d747f5b1ad0c18f59c2ed49ecde0c0
tati oliveira <tat1roliv@gmail.com>
2022-07-03 19:53:24 -0300

    form

--- event-platform/src/Router.tsx
+++ event-platform/src/Router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Evento } from "./pages/Evento";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
     return(
         <Routes>
-            <Route path="/" element={<h1>home</h1>}/>
+            <Route path="/" element={<Subscribe />}/>
             <Route path="/evento" element={ <Evento /> }/>
             <Route path="/evento/lesson/:slug" element={ <Evento /> }/>
         </Routes>
--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -1,6 +1,37 @@
+import { Logo } from '../components/Logo';
 
 export function Subscribe(){
     return(
-        <h1>Subscribe</h1>
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[640px]">
+                    <Logo />
+                    <h1 className="mt-8 text-[2.5rem] leading-tight">Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong></h1>
+                    <p className="mt-4 text-gray-200 leading-relaxed">Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.</p>
+                </div>
+
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded" >
+                    <strong>Inscreva-se gratuitamente </strong>
+                    <form action="" className="flex flex-col gap-2 w-full">
+                        <input 
+                            className="bg-gray-900 rounded px-5 h-14 mt-5"
+                            type="text" 
+                            placeholder="Digite seu nome"
+                         />
+                        <input 
+                            className="bg-gray-900 rounded px-5 h-14 "
+                            type="email" 
+                            placeholder="Digite seu e-mail" 
+                        />
+                        <button
+                        type="submit"
+                        className="bg-green-500 mt-4 py-5 uppercase rounded font-bold text-sm hover:bg-green-700">
+                            Garantir inscrição
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <img src="/src/assets/codemockup.png"  alt="imagem" className="mt-10" />
+        </div>
     )
 }
